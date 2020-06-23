6dc1c9265ddba7786088ff29e6f0d8b44d7c340e
claudiahalip <claudia4ro@yahoo.com>
2020-06-23 00:33:29 -0500

    add items code

--- garage_sale/app/controllers/item_controller.rb
+++ garage_sale/app/controllers/item_controller.rb
@@ -9,20 +9,23 @@ class ItemController < ApplicationController
       erb :'items/new'
     end 
 
+    post '/user/items' do  #create 1 itme
+      @item = Item.create(params)
+      redirect '/user/dashboard'
+    end 
+
     get '/items/:id' do  #show 1 item
         # @item = Item.find_by_id(params[:id])
         erb :'items/show'
     end 
+
+   
 
     get '/itmes/:id/edit' do #edit 1 item 
       # @item = Item.find_by_id(params[:id])
       erb :'items/edit'
     end 
 
-    post '/items' do  #create 1 itme
-      # item = Item.create(params)
-      # redirect '/itmes'
-    end 
     
     patch '/items/:id' do #update 1 item
       # item = Item.find(params[:id])
